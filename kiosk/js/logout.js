1ed7c86aedf9b2de1cf9f26d188a128ff01caf79
alexa.io <131481909+alexa0623-io@users.noreply.github.com>
2025-02-21 19:11:52 +0800

    Delete kiosk directory

--- kiosk/js/logout.js
+++
@@ -1,8 +0,0 @@
-function showLogoutModal() {
-    document.getElementById('logout-modal').classList.remove('hidden');
-}
-
-// Function to dismiss the modal
-function dismissModal() {
-    document.getElementById('logout-modal').classList.add('hidden');
-}
